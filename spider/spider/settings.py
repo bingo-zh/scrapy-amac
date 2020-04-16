d3438ce29bec1f565d1b1c48d3e133b7a17088ee
bingo_zh <luckyphoto_zh@163.com>
2020-04-16 16:38:05 +0800

    更新scrapy 相关库清单

--- spider/spider/settings.py
+++ spider/spider/settings.py
@@ -7,7 +7,7 @@ SPIDER_MODULES = ['spider.spiders']
 NEWSPIDER_MODULE = 'spider.spiders'
 IS_RANDOM_UA = 0  # 随机User-Agent
 IS_RANDOM_PROXY = 0  # 随机代理
-SPIDER_TYPE = 1  # 0:管理人(manager),1:产品(fund)
+SPIDER_TYPE = 0  # 0:管理人(manager),1:产品(fund)
 
 #USER_AGENT = 'spider (+http://www.yourdomain.com)'
 
@@ -87,69 +87,69 @@ FEED_EXPORT_ENCODING = "utf-8-sig"
 # FEED_EXPORTERS = {
 #    'csv': 'spider.spiders.AmacItemExporter.AmacItemExporter',
 # }
-FIELDS_TO_EXPORT = [
-   'ID',  # 编号
-   'FUNDNAME',  # 基金名称
-   'FUNDID',  # 基金编号
-   'CHENGLI',  # 成立时间
-   'BEIAN',  # 备案时间
-   'BEIAN_JD',  # 备案阶段
-   'FUNDTYPE',  # 基金类型
-   'BIZHONG',  # 币种
-   'FUNDMANAGE',  # 基金管理人名称
-   'MANAGETYPE',  # 管理类型
-   'TUOGUANMANAGE',  # 托管人名称
-   'STATUS',  # 运作状态
-   'LASTUPDATETIME',  # 最后更新时间
-   'REMARK',  # 特别提醒
-   'MONTH_R',  # 月报
-   'HALFY_R',  # 半年报
-   'YEAR_R',  # 年报
-   'QUARTER_R',  # 季报
-   'PARENTID'
-]
-#
 # FIELDS_TO_EXPORT = [
-#    'ID',#客户主键
-#    'MANAGERNAME',#基金管理人全称(中文)
-#    'ARTIFICIALPERSONNAME',#法定代表人/执行事务合伙人(委派代表)姓名
-#    'REGISTERNO',#登记编号
-#    'ESTABLISHDATE',#成立时间
-#    'MANAGERHASPRODUCT',#
-#    'URL',#
-#    'REGISTERDATE',#登记时间
-#    'REGISTERADDRESS',#注册地址
-#    'REGISTERPROVINCE',#注册省份
-#    'REGISTERCITY',#注册城市
-#    'REGADRAGG',#注册地区
-#    'FUNDCOUNT',#基金数量
-#    'FUNDSCALE',#基金规模
-#    'PAIDINCAPITAL',#
-#    'SUBSCRIBEDCAPITAL',#
-#    'HASSPECIALTIPS',#特别提示
-#    'INBLACKLIST',#黑名单
-#    'HASCREDITTIPS',#机构诚信信息
-#    'REGCOORDINATE',#注册坐标
-#    'OFFICECOORDINATE',#办公坐标
-#    'OFFICEADDRESS',#办公地址
-#    'OFFICEPROVINCE',#办公省份
-#    'OFFICECITY',#办公城市
-#    'PRIMARYINVESTTYPE',#机构类型
-#    'EN_MANAGERNAME',#基金管理人全称(英文)
-#    'ORG_CODE',#组织机构代码
-#    'ORG_TYPE',#企业性质
-#    'CREDITTIPS',#机构诚信信息
-#    'REGISTERPAY',#注册资本(万元)(人民币)
-#    'TUREPAY',#实缴资本(万元)(人民币)
-#    'PAYRATE',#注册资本实缴比例
-#    'BUSINESSTYPE',#注册资本实缴比例
-#    'STAFF',#员工人数
-#    'ISVIP',#是否为会员
-#    'VIPTYPE',#当前会员类型
-#    'VIPTIME',#入会时间
-#    'LAWSTATUS',#法律意见书状态
-#    'ISTOWORK',#是否有从业资格
-#    'WORKWAY',#资格取得方式
-#    'LAWORG',#律师事务所名称
-#    'LAWPER',#律师姓名
+#    'ID',  # 编号
+#    'FUNDNAME',  # 基金名称
+#    'FUNDID',  # 基金编号
+#    'CHENGLI',  # 成立时间
+#    'BEIAN',  # 备案时间
+#    'BEIAN_JD',  # 备案阶段
+#    'FUNDTYPE',  # 基金类型
+#    'BIZHONG',  # 币种
+#    'FUNDMANAGE',  # 基金管理人名称
+#    'MANAGETYPE',  # 管理类型
+#    'TUOGUANMANAGE',  # 托管人名称
+#    'STATUS',  # 运作状态
+#    'LASTUPDATETIME',  # 最后更新时间
+#    'REMARK',  # 特别提醒
+#    'MONTH_R',  # 月报
+#    'HALFY_R',  # 半年报
+#    'YEAR_R',  # 年报
+#    'QUARTER_R',  # 季报
+#    'PARENTID'
 # ]
+#
+FIELDS_TO_EXPORT = [
+   'ID',#客户主键
+   'MANAGERNAME',#基金管理人全称(中文)
+   'ARTIFICIALPERSONNAME',#法定代表人/执行事务合伙人(委派代表)姓名
+   'REGISTERNO',#登记编号
+   'ESTABLISHDATE',#成立时间
+   'MANAGERHASPRODUCT',#
+   'URL',#
+   'REGISTERDATE',#登记时间
+   'REGISTERADDRESS',#注册地址
+   'REGISTERPROVINCE',#注册省份
+   'REGISTERCITY',#注册城市
+   'REGADRAGG',#注册地区
+   'FUNDCOUNT',#基金数量
+   'FUNDSCALE',#基金规模
+   'PAIDINCAPITAL',#
+   'SUBSCRIBEDCAPITAL',#
+   'HASSPECIALTIPS',#特别提示
+   'INBLACKLIST',#黑名单
+   'HASCREDITTIPS',#机构诚信信息
+   'REGCOORDINATE',#注册坐标
+   'OFFICECOORDINATE',#办公坐标
+   'OFFICEADDRESS',#办公地址
+   'OFFICEPROVINCE',#办公省份
+   'OFFICECITY',#办公城市
+   'PRIMARYINVESTTYPE',#机构类型
+   'EN_MANAGERNAME',#基金管理人全称(英文)
+   'ORG_CODE',#组织机构代码
+   'ORG_TYPE',#企业性质
+   'CREDITTIPS',#机构诚信信息
+   'REGISTERPAY',#注册资本(万元)(人民币)
+   'TUREPAY',#实缴资本(万元)(人民币)
+   'PAYRATE',#注册资本实缴比例
+   'BUSINESSTYPE',#注册资本实缴比例
+   'STAFF',#员工人数
+   'ISVIP',#是否为会员
+   'VIPTYPE',#当前会员类型
+   'VIPTIME',#入会时间
+   'LAWSTATUS',#法律意见书状态
+   'ISTOWORK',#是否有从业资格
+   'WORKWAY',#资格取得方式
+   'LAWORG',#律师事务所名称
+   'LAWPER',#律师姓名
+]
